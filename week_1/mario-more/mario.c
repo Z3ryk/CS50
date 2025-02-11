e49ce2f6e5bdd62d58f8be05091371f1a6fe1b5e
Z3ryk <yanenko.2000@mail.ru>
2025-02-11 04:52:49 +0300

    CS-17 correct mario-more

--- week_1/mario-more/mario.c
+++ week_1/mario-more/mario.c
@@ -1,31 +1,42 @@
 #include <cs50.h>
 #include <stdio.h>
 
+
 int main(void)
 {
-    int s;
+    int height;
+    char space = ' ';
+    char hash ='#';
+
     do
     {
-        s = get_int("Height: "); // задать высоту пирамидки
+        height = get_int("Height: ");
     }
-    while (s <= 0);
-    for (int i = 1; i <= s; i++)
+    while (height <= 0);
+
+    for (int i = 0; i < height; i++)
     {
-        int space = i <= s ? s - i : 0; // расчет количества пробелов
 
-        for (int j = 0; j < space; j++) // печать пробелов
+        if (i <= height)
         {
-            printf(" ");
-        }
-        for (int resh = 1; resh <= i; resh++) // печать решеток
-        {
-            printf("#");
-        }
-        printf("  "); // разделение пирамидок
 
-        for (int k = 1; k <= i; k++) // то же самое, но в обратном порядке
-        {
-            printf("#");
+            for (int j = 0; j < height - i - 1; j++)
+            {
+                printf("%c", space);
+            }
+
+            for (int h = 0; h <= i; h++)
+            {
+                printf("%c", hash);
+            }
+
+            printf("%c%c", space, space);
+
+            for (int h = 0; h <= i; h++)
+            {
+                printf("%c", hash);
+            }
+
         }
 
         printf("\n");
