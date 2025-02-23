521d3c662af0ffa5d85446e63246163563832439
Z3ryk <yanenko.2000@mail.ru>
2025-02-23 17:12:38 +0300

    CS-24 Correct Caesar

--- week_2/caesar/caesar.c
+++ week_2/caesar/caesar.c
@@ -5,38 +5,45 @@
 #include <string.h>
 
 int main(int argc, string argv[])
-
 {
     // Проверка на количество аргументов
     if (argc != 2)
     {
-        printf("Usage: ./ceasar k\n");
+        printf("Usage: ./ceasar key\n");
         return 1;
     }
 
-    // Проверка является ли аргумент буквенным
-    for (int key = 0; key < strlen(argv[1]); key++)
+    // Длину аргумента записываем в переменную
+    int argument_length = strlen(argv[1]);
+
+    // Проверка присутствуют ли в аргументе что-то кроме цифр
+    for (int i = 0; i < argument_length; i++)
     {
-        if (isalpha(argv[1][key]))
+        if (isdigit(argv[1][i]) == false)
         {
             printf("Usage: ./caesar key\n");
             return 1;
         }
     }
 
-    int key = atoi(argv[1]) % 26; // Конвертация строки в число
+    // Переводим цифры из в аргумента в число
+    // Берем остаток от деления на 26 так как в англ. языке 26 букв.
+    int key = atoi(argv[1]) % 26;
 
     // Запрос на ввод текста
     string plaintext = get_string("plaintext: ");
 
     printf("ciphertext: ");
 
+    // Присваиваемм переменной длину строки plaintext
+    int length = strlen(plaintext);
+
     // Перебор каждой буквы в тексте
-    for (int i = 0, length = strlen(plaintext); i < length; i++)
+    for (int i = 0; i < length; i++)
     {
         if (!isalpha(plaintext[i]))
         {
-            // Выводит текущий элемент массива если он не является альфой (что такое альфа?)
+            // Выводит текущий элемент массива
             printf("%c", plaintext[i]);
             continue;
         }
@@ -45,15 +52,16 @@ int main(int argc, string argv[])
         int offset = isupper(plaintext[i]) ? 65 : 97;
 
         // Вычисление того, насколько далеко текущий элемент находится от строчной "a" или заглавной
-        int pi = plaintext[i] - offset;
+        int range_to_a = plaintext[i] - offset;
 
-        // Индекс шифра
-        int ci = (pi + key) % 26;
+        // Индекс шифра (Берем расстояние о строчной или заглавной "a" и добавляем ключ)
+        int cypher_index = (range_to_a + key) % 26;
 
         // Печать символа в зашифрованом виде
-        printf("%c", ci + offset);
+        printf("%c", cypher_index + offset);
     }
 
     printf("\n");
+
     return 0;
 }
