aaaeda4228a48adefe276f80e3d601101ed87a96
Z3ryk <yanenko.2000@mail.ru>
2025-03-19 20:05:04 +0300

    CS-25 Correct Substitution

--- week_2/substitution/substitution.c
+++ week_2/substitution/substitution.c
@@ -4,127 +4,101 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Сообщение об ошибке
+const string error_message = "Usage: ./substitution key\n";
+
+// Переменная с кол-вом букв в алфавите
+const int number_of_letters = 26;
+
 int main(int argc, string argv[])
 {
     // Проверка на количество аргументов
     if (argc != 2)
     {
-        printf("Usage: ./substitution k\n");
+        printf("%s", error_message);
         return 1;
     }
 
     // Присваеваем key значение аргумента
     string key = argv[1];
-    // Приваемае check значение аргумента
-    string check = argv[1];
+
+    // Длину аргумента записываем в переменную
+    int argument_length = strlen(key);
+
+    // Проверка на количество символов в аргументе (ключ)
+    if (argument_length != number_of_letters)
+    {
+        printf("%s", error_message);
+        return 1;
+    }
 
     // Проверка является ли аргумент буквенным
-    for (int i = 0; i < strlen(argv[1]); i++)
+    for (int i = 0; i < argument_length; i++)
     {
-        if (!isalpha(argv[1][i]))
+        if (!isalpha(key[i]))
         {
-            printf("Usage: ./substitution key\n");
+            printf("%s", error_message);
             return 1;
         }
     }
 
-    // Меняем в check буквы на единый регистр для проверки смешанного регистра
-    for (int i = 0; i < 26; i++)
+    // Меняем в key буквы на единый регистр для проверки смешанного регистра
+    for (int i = 0; i < number_of_letters; i++)
     {
-        if (islower(check[i]))
-        {
-            check[i] = check[i] - 32;
-        }
+        key[i] = toupper(key[i]);
     }
 
     // Проверка на дубликаты в ключе
-    for (int i = 0; i < 26; i++)
+    bool seen[26] = {false};
+
+    for (int i = 0; i < number_of_letters; i++)
     {
-        for (int j = i + 1; j < 26; j++)
+        int index = key[i] - 'A';
+
+        if (seen[index] == false)
         {
-            if (check[i] == check[j])
-            {
-                for (int n = j - 1; n >= 0; n--)
-                {
-                    if (check[j] == check[n])
-                    {
-                        printf("Usage: ./substitution k\n");
-                        return 1;
-                    }
-                }
-            }
+            seen[index] = true;
+        }
+
+        else
+        {
+            printf("%s", error_message);
+            return 1;
         }
     }
 
-    // Проверка на количество символов в строке
-    if (strlen(argv[1]) != 26)
-    {
-        printf("Usage: ./substitution k\n");
-        return 1;
-    }
-
-    string alp = "abcdefghijklmnopqrstuvwxyz";
-    string ALP = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // Получение текста для шифрования
     string text = get_string("Text: ");
+
     printf("ciphertext: ");
 
-    // Перебор каждой буквы в тексте
-    for (int i = 0, lng = strlen(text); i < lng; i++)
+    // Шифрование текста
+    int text_length = strlen(text);
+
+    for (int i = 0; i < text_length; i++)
     {
+        // Выводит текущий элемент массива если он не является буквенным
         if (!isalpha(text[i]))
         {
-            // Выводит текущий элемент массива если он не является альфавитным
             printf("%c", text[i]);
             continue;
         }
 
-        // Перебор какая буква из текста подходит к алвафиту и замена его на букву из ключа
-        // (Строчные)
+        // Печать строчных букв
         if (islower(text[i]))
-
         {
-            for (int x = 0, length = strlen(alp); x < length; x++)
-            {
-                int z = alp[x];
-                int h = text[i];
-                if (z == h)
-                {
-                    if (islower(key[x]))
-                    {
-                        printf("%c", key[x]);
-                    }
-
-                    else
-                    {
-                        printf("%c", key[x] + 32);
-                    }
-                }
-            }
+            int index = text[i] - 'a';
+            printf("%c", key[index] + 32);
         }
 
-        // Перебор какая буква из текста подходит к алвафиту и замена его на букву из ключа
-        // (Заглавные)
-        if (!islower(text[i]))
+        // Печать прописных букв
+        else
         {
-            for (int x = 0, length = strlen(ALP); x < length; x++)
-            {
-                int z = ALP[x];
-                int h = text[i];
-                if (z == h)
-                {
-                    if (!islower(key[x]))
-                    {
-                        printf("%c", key[x]);
-                    }
-
-                    else
-                    {
-                        printf("%c", key[x] - 32);
-                    }
-                }
-            }
+            int index = text[i] - 'A';
+            printf("%c", key[index]);
         }
     }
+
     printf("\n");
     return 0;
 }
